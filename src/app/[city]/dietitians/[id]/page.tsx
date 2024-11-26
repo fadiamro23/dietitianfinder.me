212c989f3e42626129b0212375c4c4f44a58de73
fadiamro23 <alia@hebron.edu>
2024-11-26 19:49:06 +0200

    refactor: remove static export for dynamic routes

--- src/app/[city]/dietitians/[id]/page.tsx
+++ src/app/[city]/dietitians/[id]/page.tsx
@@ -20,4 +20,15 @@ export default async function DietitianProfile({ params }: Props) {
   }
 
   return <ProfessionalTemplate city={cityData.city} dietitian={dietitian} />;
+}
+
+// Add this function to generate all possible paths at build time
+export async function generateStaticParams() {
+  // You'll need to return an array of all possible combinations of city and id
+  // Example:
+  return [
+    { city: 'new-york', id: '1' },
+    { city: 'los-angeles', id: '2' },
+    // ... add all your possible combinations
+  ]
 } 
