07ebddeb79291682c690808e4b1a6773799f184d
fadiamro23 <alia@hebron.edu>
2024-11-27 12:41:00 +0200

    fix: update config for static export

--- next.config.js
+++ next.config.js
@@ -1,16 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
     domains: [
       // Add your image domains here if you're using next/image
     ],
-  },
-  experimental: {
-    outputFileTracingRoot: undefined,
-  },
-  distDir: '.next'
+  }
 }
 
 module.exports = nextConfig 
